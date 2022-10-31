28dcca7e7b3b6a2f9319fd9f930aec5f01c6e569
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-10-31 12:00:45 -0300

    posts con comentarios, like, unlike

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,6 @@
 import { TabActions } from "@react-navigation/native";
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 import { db, auth } from '../../firebase/config';
 
 class Home extends Component {
@@ -8,49 +8,69 @@ class Home extends Component {
     constructor() {
         super();
         this.state = {
-            usuarios:null
+            datosdelusuario: {},
+            posts: [],
+        }
+        
         };
-    }
-    // clickear() {
-    //     console.log("me clickearon");
-    // }
 
     componentDidMount() {
-        db.collection('posts').onSnapshot(
-            docs => {//todos datos de la colección
-                let datosUsuario = [];
-                docs.forEach(doc => { //por cada documento, quiero un doc y la función que ejecutaré por cada doc
-                    datosUsuario.push({
-                        id: doc.id,//data, dni, pero solo quiero la que corresponde al usuario actual
-                        data: doc.data(),
-                    })
-                    this.setState({
-                        usuarios: datosUsuario,
-                        loading: false
-                    })
-                })
+        //Traer datos de la db
+        db.collection('Posts').orderBy('createdAt', 'desc').limit(5).onSnapshot(
+            docs => {
+                let posteos = [];
+                docs.forEach(doc => {
+                        const id = doc.id;
+                        const data = doc.data();
+                        posteos.push({ data, id });
+                });
+                this.setState({ posts: posteos })
             }
         )
     }
 
     render() {
-
         return (
-            <View>
-             {/* <Image style={styles.image} source={{uri: this.props.image}}/>Upload post</Image> */}
-
-
-                {/* <Text>Hola mundo</Text>
-                <TouchableOpacity onPress={() => this.clickear()}>
-                    <Text>Clickeame</Text>
-                </TouchableOpacity> */}
-                {/* <Text>Nombre del usuario:{this.state.usuarios}</Text>
-                <Text>DNI:{this.state.usuarios}</Text>
-                <Text>Edad del usuario:{this.state.usuarios}</Text>
-                 */}
-            </View >
-        );
+            <View style={estilosCss().container}>
+           <TouchableOpacity onPress={() => this.props.navigation.navigate('postform')}>
+                <Text style={estilosCss().touchable}> Añadir post </Text>
+            </TouchableOpacity>
+            <Text> DNI: {this.state.datosdelusuario?.DNI} </Text>
+            <Text> Edad del usuario: {this.state.datosdelusuario?.edad} </Text>
+            <FlatList
+                data={this.state.posts}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({ item }) => <Post postData={item} />}
+            />
+        </View>
+        )
     }
 }
-
+const estilosCss = () => StyleSheet.create({
+    container: {
+        textAlign: 'center',
+        padding: 10,
+    },
+    touchable: {
+        padding: 4,
+        backgroundColor: '#ccc',
+        marginBottom: 10,
+        borderRadius: 4,
+    },
+    text: {
+        fontWeight: 'bold'
+    },
+    contadorContainer: {
+        marginVertical: 4
+    },
+    btnContador: {
+        padding: 7,
+        backgroundColor: 'rgba(0,255,0, 0.5)',
+        marginBottom: 10,
+        borderRadius: 4,
+    },
+    textContador: {
+        fontWeight: 'bold'
+    }
+})
 export default Home;
---
+++ src/screens/PostForm.js
@@ -0,0 +1,74 @@
+import React, {Component} from "react";
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import { auth, db } from "../firebase/config";
+class PostForm extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            textoPost:'',
+        }
+    }
+    onSubmit(){
+        console.log('Posteando...');
+        db.collection('Posts').add({
+            owner: auth.currentUser.email,
+            createdAt: Date.now(),
+            textoPost: this.state.textoPost
+        })
+        .then(()=>{
+            console.log('posteado funciona')
+            this.setState({
+                textoPost: ''
+            })
+            this.props.navigation.pop();
+        })
+        .catch( e => console.log(e))
+    }
+    render(){
+        return(
+            <View style={styles.formContainer}>
+            <Text>Nuevo Post</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text)=>this.setState({textoPost: text})}
+                    placeholder='Escriba aquí...'
+                    keyboardType='default'
+                    value={this.state.textoPost}
+                    />
+                <TouchableOpacity style={styles.button} onPress={()=>this.onSubmit()}>
+                    <Text style={styles.textButton}>Postear</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    formContainer:{
+        paddingHorizontal:10,
+        marginTop: 20,
+    },
+    input:{
+        height:100,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'#28A745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4,
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28A745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+})
+export default PostForm;
