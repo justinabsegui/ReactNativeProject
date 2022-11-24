017e64b62f1804720a7dda49c3bea77534e6abaf
TomasCarzolio <tcarzolio@udesa.edu.ar>
2022-11-23 22:49:37 -0300

    Logica de editar perfil, modificacion necesaria

---
+++ src/screens/Comment.js
@@ -0,0 +1,154 @@
+import { Text, View, StyleSheet, FlatList } from 'react-native'
+import React, { Component } from 'react'
+import { auth, db } from '../../firebase/config';
+import firebase from 'firebase'
+import { TextInput, TouchableOpacity } from 'react-native-web';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+
+class Comment extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            comentarios: [],
+            opinion:""
+        }
+    }
+    componentDidMount(){
+      console.log(this.props)
+      db.collection("Posts").doc(this.props.route.params.id).limit(10).onSnapshot(
+        docs => {
+          this.setState({
+            id:docs.id,
+            comentarios: docs.data().comentarios,
+           
+          },()=> console.log(this.state.comentario))
+        }
+      )
+    }
+    comentar(text){
+      db.collection("Posts").doc(this.props.route.params.id).update({
+        comentarios: firebase.firestore.FieldValue.arrayUnion({
+          owner: auth.currentUser.email,
+          cretedAt: Date.now(),
+          comentario: text
+        })
+      })
+      .then(resp => {
+        this.setState({
+            comentario: ""
+        },()=> console.log(this.state.opinion))
+    })
+    .catch(err=> console.log(err))
+  }
+    
+  render() {
+    return (
+     
+      <View style={styles.container}>
+         <TextInput 
+            style = {styles.input}
+            keyboardType = "default"
+            placeholder = "Comentar"
+            onChangeText={ (text) => this.setState({opinion: text})}
+           
+            />
+        <TouchableOpacity style={styles.registro} onPress={() => this.comentar(this.state.opinion)}>
+        <MaterialCommunityIcons name="send-circle" size={24} color="blue" />
+        </TouchableOpacity>
+         <FlatList 
+          data={this.state.comentarios}
+          keyExtractor={(item) => item.createdAt}
+          renderItem={({item}) => <Text  style={styles.subtitle}>{item.owner}: {item.comentario}</Text>}
+        />
+      
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  photo: {
+      height: 250,
+  },
+  postContainer: {
+      borderRadius: 20,
+      borderColor: 'purple',
+      borderWidth: 1,
+      borderStyle: 'solid',
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      margin: 15
+  },
+  user: {
+      color: 'purple',
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      flexWrap: 'wrap',
+      margin: 10,
+  },
+  datos: {
+      display: 'flex',
+      justifyContent: 'left',
+      alignContent: 'left',
+      flexWrap: 'wrap',
+      marginBottom: 5
+  },
+
+  like: {
+      color: 'red',
+      display: 'flex',
+      justifyContent: 'left',
+      alignContent: 'left',
+  },
+  unlike: {
+      color: 'black',
+      display: 'flex',
+      justifyContent: 'left',
+      alignContent: 'left',
+
+  },
+  pie: {
+      color: 'purple',
+      display: 'flex',
+      justifyContent: 'left',
+      alignContent: 'left',
+      flexWrap: 'wrap',
+      marginBottom: 5,
+  },
+  comentar: {
+      color: 'black',
+      display: 'flex',
+      opacity: 10,
+      justifyContent: 'center',
+      alignContent: 'center',
+      flexWrap: 'wrap',
+      borderColor: 'purple',
+      borderWidth: 1,
+      borderRadius: 20,
+      padding: 6,
+      width: 300,
+      margin: 5,
+      padding: 6,
+  },
+  comentarr: {
+      color: 'white',
+      display: 'flex',
+      opacity: 10,
+      justifyContent: 'center',
+      alignContent: 'center',
+      flexWrap: 'wrap',
+      borderRadius: 20,
+      padding: 6,
+      width: 80,
+      backgroundColor: 'purple',
+  },
+  comments: {
+      display: 'flex',
+      justifyContent: 'left',
+      alignContent: 'left',
+      flexWrap: 'wrap',
+  },
+
+})
+
+export default Comment;
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList, ScrollView, Image } from 'react-native';
 import Post from '../components/Post';
 import { db, auth } from '../firebase/config';
+import Camara from '../components/Camara';
 
 class Profile extends Component {
     constructor(props) {
@@ -12,18 +13,27 @@ class Profile extends Component {
             bio: '',
             edad: '',
             profilePic: '',
+            editProfile: false,
+            newName: '',
+            newBio: '',
+            newPassword: '',
+            checkNewPassword: '',
+            newProfilePic: '',
+            showCamera: false,
             logout: true,
             posts: [],
             borrar: false,
-            alertaBorrarMensaje: ''
+            alertaBorrarMensaje: '',
+            passwordError: '',
+            editProfile: false,
+
         }
     }
 
     componentDidMount() {
         if (auth.currentUser.email) { // Chequear que existe auth.currentUser.email
             const email = auth.currentUser.email;
-            console.log(this.props.usuario);
-            if ( this.props.usuario !== undefined) {
+            if (this.props.usuario !== undefined) {
                 db.collection('datosUsuario').where('owner', '===', this.props.usuario).onSnapshot(   // No traer todos los datos de la colección, filtrarlos al mismo tiempo que los traemos
                     docs => {//todos datos de la colección
                         let user = '';
@@ -31,14 +41,14 @@ class Profile extends Component {
                         docs.forEach(doc => {
                             //    Condicional: si las props están vacias, es tu perfil. Sino, es el de otro usuario (o es el tuyo y hay que comparar el mail con auth.currentUser.email)
                             user = doc.data();
-                           
+
                         });
 
                         this.setState({
                             email: user.owner,
                             name: user.name,
                             bio: user.bio,
-                            edad: user.edad, 
+                            edad: user.edad,
                             profilePic: user.profilePic
                         })
 
@@ -68,8 +78,8 @@ class Profile extends Component {
                         let user = '';
                         docs.forEach(doc => {
                             //    Condicional: si las props están vacias, es tu perfil. Sino, es el de otro usuario (o es el tuyo y hay que comparar el mail con auth.currentUser.email)
-                            user =  doc.data();
-        
+                            user = doc.data();
+
                         });
 
                         this.setState({
@@ -101,32 +111,67 @@ class Profile extends Component {
         }
     }
 
+    onImageUpload(url) {
+        this.setState({
+            showCamera: false,
+            newProfilePic: url
+        });
+    }
+
+    guardarCambios(){
+        const user = auth.currentUser;
+        if (this.state.newName != ''){
+            this.setState({name: newName})
+        } if (this.state.newBio != ''){
+            this.setState({bio: newBio})
+        } if (this.state.newProfilePic != ''){
+            this.setState({profilePic: newProfilePic})
+        }  if (this.state.newPassword != '' && this.state.newPassword === this.state.checkNewPassword){
+            user.updatePassword(this.state.newPassword).then(() => {
+              }).catch((error) => {
+               this.setState({passwordError: error})
+              })} else {
+                  this.setState({passwordError: 'ingrese su nueva contraseña dos veces'})
+                }
+              db.collection('datosUsuario')
+              .doc(this.props.usuario.id)
+              .update({
+                         name: this.state.name,
+                         profilePic: this.state.profilePic,
+                         bio: this.state.bio,
+                     })
+                     .then(()=>{
+                         this.setState({editProfile: false})
+                     })
+              
+    }
+
     logOut() {
         auth.signOut();
         this.props.navigation.navigate('Login')
     }
 
-    alertaBorrarMensaje(){
-        this.setState({alertaBorrarMensaje: 'Estas seguro que queres borrar este comentario?', borrar: true})
+    alertaBorrarMensaje() {
+        this.setState({ alertaBorrarMensaje: 'Está seguro que desea eliminar su perfil?', borrar: true })
     }
 
-    borrarPerfil(){
+    borrarPerfil() {
         db.collection('datosUsuario').doc(this.props.postData.id).delete()
     }
 
-    noBorrar(){
-        this.setState({alertaBorrarMensaje: '', borrar: false})
+    noBorrar() {
+        this.setState({ alertaBorrarMensaje: '', borrar: false })
     }
 
     render() {
 
         return (
             <ScrollView>
-                { //form para borrar                
+                { //form para borrar perfil                
                     this.props.usuario == auth.currentUser.email ?
                         <>
                             <TouchableOpacity onPress={() => this.alertaBorrarMensaje()}>
-                                <Text style={styles.comentarr}>Borrar posteo</Text>
+                                <Text style={styles.comentarr}>Eliminar perfil</Text>
                             </TouchableOpacity>
 
                             <Text>{this.state.alertaBorrarMensaje}</Text>
@@ -147,28 +192,109 @@ class Profile extends Component {
                         <></>
                 }
                 <View style={styles.contenedor}>
-                <Image style={styles.profilePic}
+                
+                { this.state.editProfile = false ?
+                    <Image style={styles.profilePic}
                     source={{uri: this.state.profilePic}}
                     resizeMode='contain'
                 ></Image>
-                    <Text style={styles.usuario}>{this.state.name}</Text>
+            :
+            <View>
+            <TouchableOpacity onPress={() => { this.setState({showCamera: true})}}><Text>Cambiar foto de perfil</Text></TouchableOpacity>
+            {this.state.showCamera ?
+                <Camara onImageUpload={url => this.onImageUpload(url)} />
+                : <Text style={styles.profilePic}>Foto de perfil guardada exitosamente</Text>
+             }</View>
+                }
+                    { this.state.editProfile  ?
+                      <TextInput
+                      style={styles.field}
+                      keyboardType='default'
+                      placeholder='Nuevo nombre de usuario'
+                      value={this.state.newName}
+                  />
+            :
+          
+        <Text style={styles.usuario}>{this.state.name}</Text>
+          
+                }
+                    
                 </View>
+                { this.state.editProfile ?
+            <View>
+            {this.state.passwordError !== '' ?
+            <Text style={styles.error}>Error: {this.state.passwordError}</Text>
+            :
+            <></>}
+        <TextInput
+            style={styles.field}
+            keyboardType='default'
+            placeholder='Nueva contraseña'
+            secureTextEntry={true}
+            onChangeText={text => {
+                if (text.length < 6) {
+                    this.setState({ passwordError: "la nueva contraseña es muy corta.", newPassword: text })
+                } else {
+                    this.setState({ passwordError: '', newPassword: text})
+                }
+            }
+            }
+            value={this.state.newPassword}
+        /> 
+        <TextInput
+            style={styles.field}
+            keyboardType='default'
+            placeholder='Comprobar nueva contraseña'
+            secureTextEntry={true}
+            onChangeText={text => {
+                if (text !== this.state.newPassword) {
+                    this.setState({ passwordError: "Las contraseñas no coinciden", checkNewPassword: text})
+                } else {
+                    this.setState({ passwordError: '', checkNewPassword: text})
+                }
+            }
+            }
+            value={this.state.checkNewPassword}
+        /></View>
+               
+    
+                :
+                <></>}
+                
                     <Text style={styles.info}>Email:{this.state.email}</Text>
-                    <Text style={styles.info}>Bio:{this.state.bio}</Text>
+                   
+                    { this.state.editProfile ?
+                    <TextInput
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='Nueva Bio'
+                    onChangeText={text => this.setState({ newBio: text })}
+                    value={this.state.newBio}
+                />
+            :
+            <Text style={styles.info}>Bio:{this.state.bio}</Text>
+                }
                     <Text style={styles.info}>Age:{this.state.edad}</Text>
                     
-                {/* logout */}
-                    <TouchableOpacity onPress={() => {
-                    if (auth.currentUser.email == this.state.email) {
-                        this.logOut()
-                    } else {
-                        this.setState({ logout: false })
-                    }
-                }}>
-                    <Text style={styles.logout}>Logout</Text>
+                { this.state.editProfile ?
+                <TouchableOpacity onPress={() => this.guardarCambios() }>
+                    <Text style={styles.logout}>Guardar cambios</Text>
                 </TouchableOpacity>
+                :
+                <></>}
 
-                {/* mis posteos */}
+                {/* logout */ }
+        <TouchableOpacity onPress={() => {
+            if (auth.currentUser.email == this.state.email) {
+                this.logOut()
+            } else {
+                this.setState({ logout: false })
+            }
+        }}>
+            <Text style={styles.logout}>Logout</Text>
+        </TouchableOpacity>
+
+        {/* mis posteos */ }
                     <Text style={styles.info}>Mis posteos: </Text>
                 
                 <FlatList
@@ -181,21 +307,21 @@ class Profile extends Component {
     }
 }
 const styles = StyleSheet.create({
-    contenedor:{
+    contenedor: {
         display: 'flex',
         flexWrap: 'wrap',
         flexDirection: 'row',
-        alignContent:"flex-start",
-        justifyContent:'space-around',
-        margin:5,
+        alignContent: "flex-start",
+        justifyContent: 'space-around',
+        margin: 5,
     },
-    usuario:{
+    usuario: {
         display: 'flex',
         justifyContent: 'center',
         alignContent: 'center',
         marginBottom: 20,
         fontSize: 30,
-        color:'purple',
+        color: 'purple',
         margin: 40,
     },
     info: {
@@ -205,9 +331,9 @@ const styles = StyleSheet.create({
         alignContent: 'left',
         flexWrap: 'wrap',
         marginBottom: 5,
-        marginLeft:50,
+        marginLeft: 50,
     },
-   profilePic: {
+    profilePic: {
         height: 100,
         display: 'flex',
         justifyContent: 'center',
@@ -224,9 +350,9 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexWrap: 'wrap',
         opacity: 10,
-        marginLeft:50,
-        marginTop:25,
-        marginBottom:25,
+        marginLeft: 50,
+        marginTop: 25,
+        marginBottom: 25,
         justifyContent: 'center',
         alignItems: 'flex-start',
         borderRadius: 20,
